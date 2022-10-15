fb2e534d45fe74d798ead850ce2d95da2b2e7ea5
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-10-15 10:34:44 +0300

    adding structure files

---
+++ lib/main_tasks_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainTasksScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold
+     (
+     appBar: AppBar(
+     title: Text('Hello Sabah,',
+       style: TextStyle(color: Colors.black),),
+     backgroundColor: Colors.white,),
+     body: Column(children: [
+
+
+
+     ],),
+     //bottomNavigationBar: ,
+
+
+     );
+
+  }
+
+}
