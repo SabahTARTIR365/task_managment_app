802a994098c25bdcac24d493a2d96f0e3a11b33a
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-10-15 12:16:38 +0300

    adding bottomNavigationBar

--- lib/main_tasks_screen.dart
+++ lib/main_tasks_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MainTasksScreen extends StatelessWidget{
+class MainTasksScreen extends StatefulWidget{
+  @override
+  State<MainTasksScreen> createState() => _MainTasksScreenState();
+}
+
+class _MainTasksScreenState extends State<MainTasksScreen> {
   @override
   Widget build(BuildContext context) {
    return Scaffold
@@ -15,11 +20,29 @@ class MainTasksScreen extends StatelessWidget{
 
 
      ],),
+     bottomNavigationBar: Container(
+       margin:const EdgeInsets.all(30),//height:60,
+
+    child: ClipRRect(
+    borderRadius:  BorderRadius.circular(20),
+       child: BottomNavigationBar(
+         selectedItemColor:Color(0xFF6637E5),
+         unselectedItemColor: Color(0xFFA18CD9),
+         items: [
+           BottomNavigationBarItem(icon: Icon(Icons.home,), label: 'Home',
+             backgroundColor: Color(0xFFEAE4FE),),
+           BottomNavigationBarItem(icon: Icon(Icons.library_books),label: 'Projects'),
+           BottomNavigationBarItem(icon: Icon(Icons.alarm_outlined),label: 'Reminders'),
+           BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile')
+
+         ],
+       ),
+    ),
+     ),
      //bottomNavigationBar: ,
 
 
      );
 
   }
-
 }
